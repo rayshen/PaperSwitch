23ba03252e4bacfeede97d1ef1dc2bbda28406cc
rayshen <rayshen1012@gmail.com>
2015-08-08 12:59:23 +0800

    1

---
+++ PaperSwitchDemo/PaperSwitchDemoTests/PaperSwitchDemoTests.swift
@@ -0,0 +1,40 @@
+//
+//  PaperSwitchDemoTests.swift
+//  PaperSwitchDemoTests
+//
+//  Created by Ramotion on 27/11/14.
+//  Copyright (c) 2014 Ramotion. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PaperSwitchDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com 
